e076c40c07b3e6beed99fd43ac8d94926cbad6cc
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-27 13:12:41 +0300

    Added get user methods.

--- server/web-admin-api.js
+++ server/web-admin-api.js
@@ -3,5 +3,44 @@ Define an object to interact with API Umbrella Admin API
 Documentation site: http://apiumbrella.io/docs/admin-api/
 */
 apiUmbrellaWeb = {
+  "baseUrl": Meteor.settings.apiUmbrella.base_url,
+  "requestHeaders": {
+    "X-Api-Key": Meteor.settings.apiUmbrella.api_key,
+    "X-Admin-Auth-Token": Meteor.settings.apiUmbrella.auth_token,
+  },
+  getUser: function (userId) {
+    /*
+    Get a specific user from API Umbrella Admin API
+    return the JSON user object or error
+    */
+    try {
+      var response = HTTP.get(baseUrl, {
+        headers: requestHeaders,
+        params: {
+          "id": userId
+        }
+      });
+    } catch (error) {
+      return error;
+    }
 
+    // If successful, return the response object
+    return response;
+  },
+  getUsers: function () {
+    /*
+    Get all users from API Umbrella Admin API
+    return the JSON user object or error
+    */
+    try {
+      var response = HTTP.get(baseUrl, {
+        headers: requestHeaders,
+      });
+    } catch (error) {
+      return error;
+    }
+
+    // If successful, return the response object
+    return response;
+  }
 };
