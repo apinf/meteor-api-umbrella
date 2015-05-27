3c80498cc261ee3395baadc443009438b7c65c73
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-27 13:39:00 +0300

    Require HTTP.

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: "brylie:api-umbrella",
   summary: "Wrapper for API Umbrella Admin API.",
-  version: "0.1.2",
+  version: "0.1.3",
   git: "https://github.com/brylie/meteor-api-umbrella"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("1.0.1");
-  api.use(["meteor"]);
+  api.use(["meteor", "http"]);
 
   api.addFiles("server/web-admin-api.js", ["server"]);
 
-  api.export("apiUmbrella", ["server"]);
+  api.export("apiUmbrellaWeb", ["server"]);
 });
 
 Package.onTest(function (api) {
