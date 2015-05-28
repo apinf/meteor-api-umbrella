6b8e55e1be410270626e0f622ef193efe2f3cc0a
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-05-28 15:14:05 +0300

    Version 1.0.9.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "brylie:api-umbrella",
   summary: "Wrapper for API Umbrella Admin API.",
-  version: "0.1.8",
+  version: "0.1.9",
   git: "https://github.com/brylie/meteor-api-umbrella"
 });
 
