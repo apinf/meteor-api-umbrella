9d5f25529a60f26f8093982ba4cbb6abb8069430
Brylie Christopher Oxley <brylie@gnumedia.org>
2015-06-04 15:48:27 +0300

    Replace 'this' with'apiUmbrellaWeb'

--- server/admin-api/v1/apiUsers.js
+++ server/admin-api/v1/apiUsers.js
@@ -8,9 +8,9 @@ var apiUsers = {
   */
   getUser: function (id) {
     // Set up request url with ID
-    var url = this.baseUrl + "v1/users/" + id;
+    var url = apiUmbrellaWeb.baseUrl + "v1/users/" + id;
 
-    return this.getResponseObject(url);
+    return apiUmbrellaWeb.getResponseObject(url);
   },
   /*
   Create a new API Umbrella user
@@ -19,11 +19,11 @@ var apiUsers = {
   */
   createUser: function (userObject) {
     // Create the URL
-    var url = this.baseUrl + "v1/users/";
+    var url = apiUmbrellaWeb.baseUrl + "v1/users/";
 
     // Create the options object
     var options = {
-      headers: this.requestHeaders,
+      headers: apiUmbrellaWeb.requestHeaders,
       data: userObject
     };
 
@@ -38,9 +38,9 @@ var apiUsers = {
   */
   getUsers: function () {
     // Set up request url
-    var url = this.baseUrl + "v1/users/";
+    var url = apiUmbrellaWeb.baseUrl + "v1/users/";
 
-    return this.getResponseObject(url);
+    return apiUmbrellaWeb.getResponseObject(url);
   }
 };
 
