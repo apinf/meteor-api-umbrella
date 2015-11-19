d9cb0864d2e21b7c7e8c39fc3af118cf97a6755b
Mauricio Vieira <mauricio@mauriciovieira.net>
2015-11-19 09:10:18 -0200

    Improve ApiUmbrellaError constructor.
    does not change name to "ApiUmbrellaError" nor add http_status and backendError
    if it does not  match the expected error

--- server/apiUmbrellaError.js
+++ server/apiUmbrellaError.js
@@ -1,25 +1,34 @@
 /**
  * Define an API-Umbrella specific Error type
  * This error prototypally inherits from the Error constructor.
- * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+ * See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+ *
+ * @constructor
+ * @param {object|string} originalError - Original HTTP.call error thrown.
  */
-ApiUmbrellaError = function (messageOrError) {
-  this.name = 'ApiUmbrellaError';
-  this.message = messageOrError.message || messageOrError || 'ApiUmbrellaError';
-  // Parses HTTP.error containing a string message
+ApiUmbrellaError = function (originalError) {
+  if (originalError.message) {
+    // originalError should be an Error object, with message attribute.
+    this.message = originalError.message;
+  } else {
+    // if it is not an Error, assumes it is a string with the error message.
+    this.message = originalError;
+  }
+
+  // Parses the message returned by api-umbrella backend and captured by HTTP.call
   // Ex:
   //  'failed [422] {"errors":{"backend_protocol":["is not included in the list"]}'
   var parseErrorMessage = /failed \[(\d+)\] ({.+})/.exec(this.message);
   if (typeof(parseErrorMessage) === 'object' &&
       parseErrorMessage.length === 5) {
+    // Only changes the Error name if its message got parsed.
+    this.name = 'ApiUmbrellaError';
+
     // Assigns the http_status number, e.g. 422
     this.http_status = JSON.parse(parsedErrorMessage[1]);
 
     // Assigns the backend_errors as JSON, e.g. {"backend_protocol":["is not included in the list"]}
     this.backend_errors = JSON.parse(parsedErrorMessage[2].errors);
-  } else {
-    this.http_status = 'undefined';
-    this.backend_errors = 'undefined';
   }
   this.stack = (new Error()).stack;
 }
