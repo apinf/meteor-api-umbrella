5f9dfdd9c8c83229819ec14438371592a508fde0
Alapan Mukherjee <alapan.mukherjee@codemenders.com>
2016-03-10 11:47:00 +0200

    Version set to 1.4.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "brylie:api-umbrella",
   summary: "Wrapper for API Umbrella Admin API.",
-  version: "1.3.1",
+  version: "1.4.1",
   git: "https://github.com/apinf/meteor-api-umbrella.git"
 });
 
